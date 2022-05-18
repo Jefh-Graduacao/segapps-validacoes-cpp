18076e11b6d6dd67336ecf54ac1459f1d28eb164
jfbueno <buenojeferson@outlook.com>
2022-05-17 21:39:11 -0300

    Melhoria nas mensagens e apontamento null

--- program.cpp
+++ program.cpp
@@ -8,7 +8,7 @@ int main(int argc, const char *argv[])
 
     if (argc < 2)
     {
-        std::cout << "Informe um IPV6 como argumento do programa";
+        std::cout << "Informe um IPV6 como argumento do programa" << std::endl;
         return 1;
     }
 
@@ -22,25 +22,27 @@ int main(int argc, const char *argv[])
         std::cout << ipv6->getValue() << " é um IPv6 válido!" << std::endl;
 
         delete ipv6;
+        ipv6 = NULL;
     }
     catch (MalformedIPException malformedIpExc)
     {
         std::cout << probemPrefix << std::endl
                   << std::endl
                   << malformedIpExc.message() << std::endl;
+        ipv6 = NULL;
     }
     catch (InvalidCharException invalidCharExc)
     {
         std::cout << probemPrefix << std::endl
                   << std::endl
                   << invalidCharExc.message() << std::endl;
+        ipv6 = NULL;
     }
     catch (...)
     {
-        std::cout << "Putz, deu ruim" << std::endl;
+        std::cout << "Ocorreu algum problema..." << std::endl;
+        ipv6 = NULL;
     }
-
-    ipv6 = NULL;
 
     std::cout << std::endl
               << "Fim do programa" << std::endl;
